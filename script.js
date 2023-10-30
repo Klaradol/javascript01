f6f88e4b268625c8e02ab1f81902170ce7571949
Klaradol <145441269+Klaradol@users.noreply.github.com>
2023-10-30 14:55:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function myNumber(){
+ 
+  const inputAge = document.getElementById("num").value;
+
+  if(inputAge >= 18){
+  console.log("jste dospělý")
+}  else if(inputAge < 18){
+  console.log("jste ještě dítě")
+}
+}
+
+function myNumber1(){
+
+  const inputAge1 = document.getElementById("num1").value;
+
+  if(inputAge1 <= 2005){
+  console.log("přistup povolen")
+}  else if(inputAge1 > 2005){
+  console.log("přístup zamítnut")
+}
+}
+
+  
+   
+    
+ 
+
+
