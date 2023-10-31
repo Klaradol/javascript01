4922f795b79b479c5dd6f07641608866a19cdd20
Klaradol <145441269+Klaradol@users.noreply.github.com>
2023-10-31 13:09:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,77 +1,30 @@
+function sumNumbers(){
 
-function myNumber(){
- 
-  const inputAge = document.getElementById("num").value;
-
-  if(inputAge >= 18){
-  console.log("jste dospělý")
-}  else if(inputAge < 18){
-  console.log("jste ještě dítě")
-}
-}
-
-function myNumber1(){
-
-  const inputAge1 = document.getElementById("num1").value;
-
-  if(inputAge1 <= 2005){
-  console.log("přistup povolen")
-}  else if(inputAge1 > 2005){
-  console.log("přístup zamítnut")
-}
-}
-
-function ifAgeFunction () {
-   
-  let inputAge = parseInt(document.getElementById("inputAge").value)
-
-
-
-  if (inputAge < 18){
-      document.getElementById("outputAge").value = "Mladý uživatel"
-
-  } else if (inputAge >= 18 && inputAge < 200){
-      document.getElementById("outputAge").value = "Dospělý uživatel"
-
-  } else if( inputAge > 18 < 0) {
-         
-
-  } else if (inputAge <= 2005) {
-      document.getElementById("outputAge").value = "Jsi Dospělý"
-
-
-  } else if (inputAge > 2005){
-      document.getElementById("outputAge").value = "Jsi moc Mladý"
-
-  }
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-function forCycle(){
-
-let userInput = parseInt(document.getElementById("cycleInput").value)
-let output=""
-
-    for(let i = 0; i < userInput; i++){
-
-      output = output + "."
-
-        document.getElementById("output").innerHTML= output
-    }
-}
-   
+    let inputNumber = parseInt(document.getElementById("inputNumber").value)
+     outputNumber = 0
     
- 
+        for(let i = 0; i <= inputNumber; i++){
+    
+          outputNumber = outputNumber + i
+    
+        }
+        document.getElementById("outputNumber").innerHTML= outputNumber
+
+
+}
+
+
+function sumOddNumbers(){
+    let inputNumber = parseInt(document.getElementById("inputNumber").value)
+
+    outputNumber = 0
+
+    if (number % 2 == 1) {
+        ystem.out.println(number + " is even.");
+       
+    }
+    document.getElementById("outputNumber").innerHTML = outputNumber
+}
+
 
 
