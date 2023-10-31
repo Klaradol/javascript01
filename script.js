ca15b89fa10c5fb01c84f7d9a7acb9ad86d2fb32
Klaradol <145441269+Klaradol@users.noreply.github.com>
2023-10-31 11:51:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,7 +21,55 @@ function myNumber1(){
 }
 }
 
-  
+function ifAgeFunction () {
+   
+  let inputAge = parseInt(document.getElementById("inputAge").value)
+
+
+
+  if (inputAge < 18){
+      document.getElementById("outputAge").value = "Mladý uživatel"
+
+  } else if (inputAge >= 18 && inputAge < 200){
+      document.getElementById("outputAge").value = "Dospělý uživatel"
+
+  } else if( inputAge > 18 < 0) {
+         
+
+  } else if (inputAge <= 2005) {
+      document.getElementById("outputAge").value = "Jsi Dospělý"
+
+
+  } else if (inputAge > 2005){
+      document.getElementById("outputAge").value = "Jsi moc Mladý"
+
+  }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+function forCycle(){
+
+let userInput = parseInt(document.getElementById("cycleInput").value)
+let output=""
+
+    for(let i = 0; i < userInput; i++){
+
+      output = output + "."
+
+        document.getElementById("output").innerHTML= output
+    }
+}
    
     
  
