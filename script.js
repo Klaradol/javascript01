fa24da07981a13fd742a871f6d16b438949cb39c
Klaradol <145441269+Klaradol@users.noreply.github.com>
2023-11-06 14:59:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-function sumNumbers(){
+ function calculatorBMI() {
+     
+        const weightInput = parseFloat(document.getElementById("weight").value)
+        const heightInput = parseFloat(document.getElementById("height").value)
 
-    let inputNumber = parseInt(document.getElementById("inputNumber").value)
-     outputNumber = 0
-    
-        for(let i = 0; i <= inputNumber; i++){
-    
-          outputNumber = outputNumber + i
-    
+     
+        const BMI = weightInput / ((heightInput*heightInput)).toFixed(1)
+
+        console.log(BMI)
+     
+        if(BMI < 18.5) {
+            document.getElementById("outputWeight").value = "Podváha"
+       
+        } else if( BMI <= 18.5 && BMI < 25){
+            document.getElementById("outputWeight").value = "Normální váha"
+     
+        } else if(  BMI <= 25 && BMI < 30){
+            document.getElementById("outputWeight").value = "Nadváha"
+     
+        } else if( BMI > 30){
+            document.getElementById("outputWeight").value = "Obezita"
         }
-        document.getElementById("outputNumber").innerHTML= outputNumber
-
-
-}
-
-
-function sumOddNumbers(){
-    let inputNumber = parseInt(document.getElementById("inputNumber").value)
-
-    outputNumber = 0
-
-    if (number % 2 == 1) {
-        ystem.out.println(number + " is even.");
        
     }
-    document.getElementById("outputNumber").innerHTML = outputNumber
-}
-
-
-
