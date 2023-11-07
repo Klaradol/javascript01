cc3d306c7c40ec760151ee0e2ea0a339e6c0824e
Klaradol <145441269+Klaradol@users.noreply.github.com>
2023-11-07 13:14:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const usersData =[]
+ 
  function calculatorBMI() {
      
         const weightInput = parseFloat(document.getElementById("weight").value)
@@ -8,17 +10,35 @@
 
         console.log(BMI)
      
-        if(BMI < 18.5) {
-            document.getElementById("outputWeight").value = "Podváha"
-       
-        } else if( BMI <= 18.5 && BMI < 25){
-            document.getElementById("outputWeight").value = "Normální váha"
-     
-        } else if(  BMI <= 25 && BMI < 30){
-            document.getElementById("outputWeight").value = "Nadváha"
-     
-        } else if( BMI > 30){
-            document.getElementById("outputWeight").value = "Obezita"
+    if(weightInput <= 0){
+        document.getElementById("outputWeight").innerHTML = "Záporná hodnota" 
+        usersData.push("zaporná hodnota")
+
+    }else if(heightInput <= 0){
+        document.getElementById("outputWeight").innerHTML = "Záporná hodnota"
+    }else if (weightInput > 0 && heightInput > 0){
+        
+    if(BMI < 18.5) {
+            document.getElementById("outputWeight").innerHTML = "Podváha"
+            usersData.push("podváha")
+
+    } else if( BMI <= 18.5 && BMI < 25){
+            document.getElementById("outputWeight").innerHTML = "Normální váha"
+     usersData.push("normální váha")
+
+    } else if(  BMI <= 25 && BMI < 30){
+            document.getElementById("outputWeight").innerHTML = "Nadváha"
+     usersData.push("nadváha")
+
+    } else if( BMI > 30){
+            document.getElementById("outputWeight").innerHTML = "Obezita"
+            usersData.push("obezita")
         }
-       
     }
+
+console.log ()
+    document.getElementById("arrayOutput").innerHTML = usersData
+ }
+
+
+  
